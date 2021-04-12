8bfff2a23d4996443d88ece6c9c3c4c4c2da6ece
Luke Whrit <lukewhrit@gmail.com>
2021-04-12 01:07:19 -0400

    fix(commands/fun): remove extra license headers from cat and duck command files

--- commands/fun/duck.go
+++ commands/fun/duck.go
@@ -15,12 +15,6 @@ import (
 	"github.com/zekroTJA/shireikan"
 )
 
-/*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
-
 type duckResponse struct {
 	Message string `json:"message"`
 	URL     string `json:"url"`
